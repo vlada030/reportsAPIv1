305051d719ab29ea8692d1eb181a36fe26462f19
Vlada DELL <vlada030@gmail.com>
2020-09-15 21:40:31 +0200

    prebacivanje na REST API sa SSRa

---
+++ routes/authRoute.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { check, body} = require('express-validator');
+
+const {register, login, getMe, deleteMe, logout, logoutAll, updateDetails, updatePassword, forgotPassword, updateAvatar, deleteAvatar, resetPassword, uploadUserPhoto, getAvatar} = require('../controllers/authController');
+
+const router = express.Router();
+
+// importuj middleware za autentikaciju
+const { protect } = require('../middleware/auth');
+
+router
+    .route("/register")
+    .post(
+        [
+            check("name")
+                .not()
+                .isEmpty()
+                .withMessage("Unesite korisničko ime")
+                .trim(),
+            check("email")
+                .isEmail()
+                .withMessage("Unesite ispravnu email adresu")
+                .normalizeEmail(),
+            check(
+                "password",
+                "Šifra treba da sadrži slova i brojeve, 7 - 15 karaktera"
+            )
+                .isLength({ min: 7, max: 15 })
+                .isAlphanumeric(),
+            body("confirmPassword").custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Šifre se ne podudaraju, ponovite unos");
+                }
+                return true;
+            }),
+        ],
+        register
+    );
+router.route("/login").post(login);
+router.route("/me").get( getMe).delete(deleteMe);
+router
+    .route("/me/avatar")
+    .get(getAvatar)
+    .post(uploadUserPhoto, updateAvatar)
+    .delete(deleteAvatar);
+router.post("/logout", protect, logout);
+router.post("/logoutAll", protect, logoutAll);
+router.put("/update", updateDetails);
+router.put("/updatepassword", updatePassword);
+router.post("/resetpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const colors = require('colors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const formidableMiddleware = require('express-formidable');
+//const formidableMiddleware = require('express-formidable');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 const cors = require('cors');
 
 //import custom error handlera
 const errorHandler = require('./middleware/errorHandler');
+
 // import MONGO konekcije 
 const connectDB = require('./config/db');
+
 // import routa
-const products = require('./routes/products');
-const users = require('./routes/auth');
+const reportsRoute = require('./routes/reportsRoute');
+const productsRoute = require('./routes/productsRoute');
+const authRoute = require('./routes/authRoute');
+const usersRoute = require('./routes/usersRoute');
 
 // ENV fajl nije u root folderu zato mora da se navede putanja
 dotenv.config({
@@ -28,13 +37,13 @@ const app = express();
 
 // pozivanje defaultnog body parsera za dobijanje req.body
 // ako je req.body u vidu objects ili arrays
-// app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}));
 // ako je req.body JSON format
-//app.use(express.json());
+app.use(express.json());
 
 // kada se podaci salju preko formData, formData automatski setuje 'content-type': 'multipart/form-data' koji bodyparser iznad ne moze da iscita
 // formidable moze da iscita i json type tako da body parser ne treba
-app.use(formidableMiddleware());
+//app.use(formidableMiddleware());
 
 // pozivanje cookie parsera da bi u cookie mogao sa se ubaci token
 app.use(cookieParser());
@@ -47,12 +56,40 @@ if (process.env.NODE_ENV === 'development') {
 // Mongo sanitize - No-SQL injection
 app.use(mongoSanitize());
 
+// dodatno setovanje security headera
+//app.use(helmet());
+
+// prevencija XSS - input sadrzi script tag ili html koji se kasnije prilikom prikazivanja moze ucitati u stranu, pre routa se poziva
+app.use(xss());
+
+// ogranicavanje broja requesta serveru
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+//app.set('trust proxy', 1);
+ 
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 400 // limit each IP to 100 requests per windowMs
+  });
+   
+//  apply to all requests
+app.use(limiter);
+
+// spreci HPP (http parameter polution)
+app.use(hpp());
+
 // CORS
 app.use(cors());
 
+// pravljenje static foldera
+app.use(express.static(path.join(__dirname, 'public')));
+
 // postavljanje routa
-app.use('/api/v1/products', products);
-app.use('/api/v1/auth', users);
+app.use('/api/v1/reports', reportsRoute);
+app.use('/api/v1/products', productsRoute);
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', usersRoute);
+
 // pozivanje custom errorHandlera koji se inicijalizira preko next iz controllera
 app.use(errorHandler);
 
